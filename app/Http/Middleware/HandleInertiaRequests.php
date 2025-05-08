f1a668c8a22c635ee608b5beb6dad36fe9bffbc5
lindltaylor7 <riuyagami@gmail.com>
2025-05-08 10:15:11 -0500

    fix laravel

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,14 +40,17 @@ class HandleInertiaRequests extends Middleware
     {
         [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
 
+        $user = $request->user();
+
         return [
             ...parent::share($request),
             'name' => config('app.name'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
-            'auth' => [
-                'user' => $request->user(),
-                'permissions' => User::find($request->user()->id)->getAllPermissions()
-            ],
+            'auth' => $user ? [
+                'user' => $user,
+                'permissions' => $user->getAllPermissions(),
+                'roles' => $user->getRoleNames(),
+            ] : null,
             'ziggy' => [
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
