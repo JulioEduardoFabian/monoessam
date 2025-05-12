dfb8b45d930c92d053f0a561ebf81c2a52dc59a7
Julio Eduardo Fabian <julio.edward.fabian@gmil.com>
2025-05-12 16:39:19 -0500

    primera migracion

---
+++ database/migrations/2025_05_12_162203_create_ingredients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('presentation')->nullable();
+            $table->string('amount')->nullable();
+            $table->string('measurement_unit')->nullable();
+            $table->decimal('calories',7,2)->nullable();
+            $table->decimal('liquid_waste',5,4)->nullable();
+            $table->decimal('solid_waste',5,4)->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('ingredient_categories')->cascadeOnUpdate()->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredients');
+    }
+};
